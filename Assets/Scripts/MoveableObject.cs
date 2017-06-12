ac22770f9d8b04e495597d5828e4d7c9b7f6166b
Der-Die-Das <der-die-das@hotmail.com>
2017-06-12 10:29:29 +0200

    - Created Elevator Model

---
+++ Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableObject : MonoBehaviour
+{
+    public float range;
+    private bool grabbed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
