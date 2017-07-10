bc564ccf13d0cdf8c840b179faf62cc375c0203f
Ofaso <kevinwuestner@gmail.com>
2017-07-10 13:07:26 +0200

    Created Elevator functionality

---
+++ Assets/Scripts/VRSensor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRSensor : MonoBehaviour
+{
+    public System.Action<GameObject> triggerEnter;
+    public System.Action<GameObject> triggerLeave;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnter(other.gameObject);
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        triggerLeave(other.gameObject);
+    }
+}
