33b645b0ffeb5ce80d5f726a2521c8345bbd191c
Der-Die-Das <der-die-das@hotmail.com>
2017-07-25 12:15:27 +0200

    Script for Directional Light

---
+++ Assets/Scripts/Night.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Night : MonoBehaviour {
+    private float xRotation;
+    private Light Sun;
+    public float transition = 0.01f;
+    public float sunRise = 340;
+    public float sunSet = 270;
+
+    private void Start()
+    {
+        Sun = this.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        xRotation = transform.eulerAngles.x;
+        if(xRotation > sunSet && xRotation < sunRise) {
+            if(Sun.intensity > 0)
+            {
+                Sun.intensity -= transition;
+            }
+        }
+        else
+        {
+            if(Sun.intensity < 1)
+            {
+                Sun.intensity += transition;
+            }
+        }
+    }
+}
